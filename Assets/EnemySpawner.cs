c445ec51db9b9afbadf4c00645c47bd65f510991
Cem <acertekin@gmail.com>
2022-05-28 14:55:27 -0400

    Small adjustment to spawner script

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -13,6 +13,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject spawner_5;
     public GameObject liptank;
     public GameObject infected;
+    public GameObject spawnedEnemyParent;
 
     void Awake()
     {
@@ -22,14 +23,25 @@ public class EnemySpawner : MonoBehaviour
 
     // Update is called once per frame
 
-   
+    void OnDisable()
+    {
+        killMonsters(spawnedEnemyParent.transform);
+    }
 
     void spawnMonsters()
     {
-        Instantiate(infected, spawner_1.transform.position,spawner_1.transform.rotation);
-        Instantiate(infected, spawner_2.transform.position, spawner_2.transform.rotation);
-        Instantiate(infected, spawner_3.transform.position, spawner_3.transform.rotation);
-        Instantiate(liptank, spawner_4.transform.position, spawner_4.transform.rotation);
-        Instantiate(infected, spawner_5.transform.position, spawner_5.transform.rotation);
+        Instantiate(infected, spawner_1.transform.position,spawner_1.transform.rotation, spawnedEnemyParent.transform);
+        Instantiate(infected, spawner_2.transform.position, spawner_2.transform.rotation, spawnedEnemyParent.transform);
+        Instantiate(infected, spawner_3.transform.position, spawner_3.transform.rotation, spawnedEnemyParent.transform);
+        Instantiate(liptank, spawner_4.transform.position, spawner_4.transform.rotation, spawnedEnemyParent.transform);
+        Instantiate(infected, spawner_5.transform.position, spawner_5.transform.rotation, spawnedEnemyParent.transform);
+    }
+
+    void killMonsters(Transform spawnParent)
+    {
+        foreach(GameObject child in spawnParent)
+        {
+            Destroy(child);
+        }
     }
 }
