ae194d74383241466b9bc50e9fd08f820963c4ab
Cem <acertekin@gmail.com>
2022-05-31 18:36:49 -0400

    Changed the health spawns in boss

--- Assets/HealthSpawner.cs
+++ Assets/HealthSpawner.cs
@@ -14,7 +14,9 @@ public class HealthSpawner : MonoBehaviour
     public GameObject grenade_pickup;
     public GameObject fuel_pickup;
     public GameObject pickupParent;
+    public GameObject player;
     public bool pickupsPresent = false;
+    public bool playerFullHealth = true;
     [Header("Timer")]
     public float phase_timer = 60f;
     public float timer;
@@ -22,23 +24,44 @@ public class HealthSpawner : MonoBehaviour
 
 
     // Update is called once per frame
-    private void Start()
+    private void Awake()
     {
         timer = phase_timer;
         spawnPickups();
+        player = GameObject.FindWithTag("Player");
         pickupsPresent = true;
+        Debug.Log(player.GetComponent<CharacterController2D>().life);
+        Debug.Log("AYYYOOOO");
+        if (player.GetComponent<CharacterController2D>().life < 4)
+        {
+            playerFullHealth = false;
+        }
+        if (player.GetComponent<CharacterController2D>().life == 4)
+        {
+            playerFullHealth = true;
+        }
     }
     private void Update()
     {
         timer -= Time.deltaTime;
+        if (player.GetComponent<CharacterController2D>().life == 4)
+        {
+            playerFullHealth = true;
+        }
+        if (player.GetComponent<CharacterController2D>().life < 4)
+        {
+            playerFullHealth = false;
+        }
         if (timer <= trigger_time)
         {
             checkPickups(pickupParent.transform);
-            if (pickupsPresent == false)
+            if (pickupsPresent == false && playerFullHealth == false)
             {
                 spawnPickups();
                 pickupsPresent = true;
+                timer = phase_timer;
             }
+            
         }
     }
 
@@ -46,8 +69,8 @@ public class HealthSpawner : MonoBehaviour
     {
         Instantiate(health_pickup, spawner_1.transform.position, spawner_1.transform.rotation, pickupParent.transform);
         Instantiate(health_pickup, spawner_2.transform.position, spawner_2.transform.rotation, pickupParent.transform);
-        Instantiate(grenade_pickup, spawner_3.transform.position, spawner_3.transform.rotation, pickupParent.transform);
-        Instantiate(fuel_pickup, spawner_4.transform.position, spawner_4.transform.rotation, pickupParent.transform);
+        Instantiate(health_pickup, spawner_3.transform.position, spawner_3.transform.rotation, pickupParent.transform);
+        Instantiate(health_pickup, spawner_4.transform.position, spawner_4.transform.rotation, pickupParent.transform);
     }
 
 
