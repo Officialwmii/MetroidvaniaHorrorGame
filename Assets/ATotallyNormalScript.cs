4c5d1d922217170de2f8f3ffd3c1788eb1b6238f
ristoretto <ristoretto@gmail.com>
2022-06-03 00:13:08 +0200

    Importing boss sprites and prefabs and nothing special

---
+++ Assets/ATotallyNormalScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ATotallyNormalScript : MonoBehaviour
+{
+
+    public GameObject toActivate;
+    public float delayTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Enter");
+
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            StartCoroutine(SpawnEnemy());
+
+        }
+    }
+    IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(delayTimer);
+
+        toActivate.SetActive(true);      
+        Destroy(gameObject);
+
+    }
+
+}
