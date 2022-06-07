7d15a491286eae8e4c9987ba1954ff6fe8478daa
Officialwmii <Officialwaynemii@gmail.com>
2022-06-07 00:17:02 -0400

    Meta files

--- Assets/Scripts/AmbienceAreaBoundary.cs
+++ Assets/Scripts/AmbienceAreaBoundary.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AmbienceAreaBoundary : MonoBehaviour
 {
     public string areaName;
+
     private GameObject player;
 
     public void PlayAreaAmbience(string area)
