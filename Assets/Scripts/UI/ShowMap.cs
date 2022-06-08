3749c70086ac033f6c240e014071812ad6146836
Cem <acertekin@gmail.com>
2022-06-08 00:18:22 -0400

    Disable minimap when player in boss arena

--- Assets/Scripts/UI/ShowMap.cs
+++ Assets/Scripts/UI/ShowMap.cs
@@ -6,31 +6,39 @@ public class ShowMap : MonoBehaviour
 {
 
     static public bool showMap = true;
-
+    public GameObject BossArenaBounds;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BossArenaBounds = GameObject.Find("BossArenaBounds");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("ShowMap")) {
+        if (BossArenaBounds.GetComponent<BossArena>().PlayerInArena == true)
+        {
+            showMap = false;
+        }
 
-            if (showMap == true)
+        if (BossArenaBounds.GetComponent<BossArena>().PlayerInArena == false)
+        {
+            if (Input.GetButtonDown("ShowMap"))
             {
-                Debug.Log("press map show");
 
-                showMap = false;
+                if (showMap == true)
+                {
+                    Debug.Log("press map show");
+
+                    showMap = false;
+                }
+                else
+                {
+                    Debug.Log("press map hide");
+
+                    showMap = true;
+                }
             }
-            else {
-                Debug.Log("press map hide");
-
-                showMap = true;
-            }
-
-
 
         }
     }
