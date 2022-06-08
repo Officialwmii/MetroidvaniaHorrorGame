ef29e62dc60c27c9df5f536940045c13f8314337
Cem <acertekin@gmail.com>
2022-06-08 01:06:39 -0400

    Skip to boss cheat implemented (press 8, 9, 0 at the same time)

---
+++ Assets/CheatManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Player;
+    public GameObject CheatPlayerSpawn;
+
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        CheatPlayerSpawn = GameObject.Find("CheatPlayerSpawn");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha8) && Input.GetKeyDown(KeyCode.Alpha9) && Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Player.transform.position = CheatPlayerSpawn.transform.position;
+            EventManager.HasArmour = true;
+            EventManager.HasDoubleJetpack = true;
+            EventManager.HasJetpack = true;
+            EventManager.HasRocketLauncher = true;
+            EventManager.FuelRefillNumberOfUpgrades = 4;
+            EventManager.ConstalationsKeysAcquired = 3;
+
+        }
+    }
+}
