7b93840c6dba205515c2d7ededf05e455d18fee4
ristoretto <ristoretto@gmail.com>
2022-06-18 14:28:04 +0200

    Gas deals damage every 0.35s instead of once and instant

--- Assets/Scripts/GasDamage.cs
+++ Assets/Scripts/GasDamage.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GasDamage : MonoBehaviour
 {
+
+    private bool PlayerIsOverlapping = false;
+    private float OverlappingTimer=0;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,22 +15,44 @@ public class GasDamage : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (PlayerIsOverlapping) {
+
+            OverlappingTimer = OverlappingTimer + Time.deltaTime;
+
+            if (OverlappingTimer >= 0.35f) {
+
+                player.GetComponent<CharacterController2D>().ApplyDamage(-1);
+                OverlappingTimer = 0;
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player") && EventManager.HasArmour==false)
         {
-            col.gameObject.GetComponent<CharacterController2D>().ApplyDamage(-1);
+            player = col.gameObject;
+            PlayerIsOverlapping = true;
+            OverlappingTimer = 0;
         }
         else if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "tilemap" || col.gameObject.tag == "Jumpthrough")
         {
             Physics2D.IgnoreCollision(col.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
         }
-    }        
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+
+        if (col.CompareTag("Player"))
+        {
+            PlayerIsOverlapping = false;
+        }
+
+
+
+    }
 
 
 }
