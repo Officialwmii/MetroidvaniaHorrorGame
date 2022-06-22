6567777ff2b2e6c1a629003eedf0b0c33d95600d
ristoretto <ristoretto@gmail.com>
2022-06-22 18:00:59 +0200

    Removing special character for "..." in subtitle script

--- Assets/Scripts/Audio/SubtitlesText.cs
+++ Assets/Scripts/Audio/SubtitlesText.cs
@@ -41,7 +41,7 @@ public class SubtitlesText : MonoBehaviour
                 switch (CurrentLine){
                     case 1: tempsub = "Playback of audiolog #"+ AudioLogNr;  LineDuration = 4f; break;
                     case 2: tempsub = "Listen. [pained grunt]"; LineDuration = 2.5f;  break;
-                    case 3: tempsub = "Starlight. I… can control it still. I…"; LineDuration = 3.2f; break;
+                    case 3: tempsub = "Starlight. I... can control it still. I..."; LineDuration = 3.2f; break;
                     case 4: tempsub = "What we found on Vigil Prime cannot leave this vessel."; LineDuration = 3.3f; break;
                     case 5: tempsub = "Find the keys. Initiate the scuttle."; LineDuration = 3.25f; break;
                     case 6: tempsub = "Save… [pained scream]."; LineDuration = 3.4f; break;
@@ -52,7 +52,7 @@ public class SubtitlesText : MonoBehaviour
                 {
                     case 1: tempsub = "Playback of audiolog #" + AudioLogNr; LineDuration = 4f; break;
                     case 2: tempsub = "Crissa, Starlight, I need you to understand why I did what I did."; LineDuration = 3f; break;
-                    case 3: tempsub = "I had no choice. This whole thing is a nightmare and…"; LineDuration = 4.5f; break;
+                    case 3: tempsub = "I had no choice. This whole thing is a nightmare and..."; LineDuration = 4.5f; break;
                     case 4: tempsub = "and I have no one else to talk to. No one left to listen."; LineDuration = 7f; break;
                     case 5: tempsub = "So, lend me your ear."; LineDuration = 2f; break;
                 }
@@ -62,7 +62,7 @@ public class SubtitlesText : MonoBehaviour
                 switch (CurrentLine)
                 {
                     case 1: tempsub = "Playback of audiolog #" + AudioLogNr; LineDuration = 4f; break;
-                    case 2: tempsub = "So much blood. They…"; LineDuration = 2f; break;
+                    case 2: tempsub = "So much blood. They..."; LineDuration = 2f; break;
                     case 3: tempsub = "The quarantine didn’t help."; LineDuration = 3f; break;
                     case 4: tempsub = "They ripped Kris to shreds. "; LineDuration = 4f; break;
                     case 5: tempsub = "So we shot them. I shot them."; LineDuration = 2f; break;
@@ -75,11 +75,11 @@ public class SubtitlesText : MonoBehaviour
                 switch (CurrentLine)
                 {
                     case 1: tempsub = "Playback of audiolog #" + AudioLogNr; LineDuration = 4f; break;
-                    case 2: tempsub = "I… I forget how many sectors we have on this vessel."; LineDuration = 3.5f; break;
+                    case 2: tempsub = "I... I forget how many sectors we have on this vessel."; LineDuration = 3.5f; break;
                     case 3: tempsub = "I’ve forgotten the faces of the ones I killed."; LineDuration = 2.5f; break;
                     case 4: tempsub = "But not their screams."; LineDuration = 3.5f; break;
-                    case 5: tempsub = "What is? Why did…"; LineDuration = 2.5f; break;
-                    case 6: tempsub = "What… What happened?"; LineDuration = 2.5f; break;
+                    case 5: tempsub = "What is? Why did..."; LineDuration = 2.5f; break;
+                    case 6: tempsub = "What.. What happened?"; LineDuration = 2.5f; break;
                     case 7: tempsub = "No, Starlight. I must focus."; LineDuration = 5f; break;
                 }
                 break;
@@ -93,7 +93,7 @@ public class SubtitlesText : MonoBehaviour
                     case 4: tempsub = "But I know you, Starlight."; LineDuration = 2.5f; break;
                     case 5: tempsub = "And I know there is no other option."; LineDuration = 2f; break;
                     case 6: tempsub = "Me, the bosun, the chief engineer, we are compromised."; LineDuration = 4f; break;
-                    case 7: tempsub = "That’s why the keys… [recording abruptly ends]"; LineDuration = 2f; break;
+                    case 7: tempsub = "That’s why the keys... [recording abruptly ends]"; LineDuration = 2f; break;
                 }
                 break;
             case 6:
