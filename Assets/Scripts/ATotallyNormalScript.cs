0859adf33d333ff7041fc3b4d9b8678e766383af
Officialwmii <Officialwaynemii@gmail.com>
2022-07-18 13:35:56 -0400

    ALOT of Audio Fixes and added some SFX
    Crate falling and jump scare need to be implemented

--- Assets/Scripts/ATotallyNormalScript.cs
+++ Assets/Scripts/ATotallyNormalScript.cs
@@ -7,6 +7,8 @@ public class ATotallyNormalScript : MonoBehaviour
 
     public GameObject toActivate;
     public float delayTimer;
+    public AudioSource jumpScare;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,16 +29,15 @@ public class ATotallyNormalScript : MonoBehaviour
 
             this.GetComponent<BoxCollider2D>().enabled = false;
             StartCoroutine(SpawnEnemy());
-
         }
     }
+
     IEnumerator SpawnEnemy()
     {
         yield return new WaitForSeconds(delayTimer);
 
-        toActivate.SetActive(true);      
+        toActivate.SetActive(true);
         Destroy(gameObject);
 
     }
-
 }
